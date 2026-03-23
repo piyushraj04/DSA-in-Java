691a58990ef3fc6796dcc8b6734bc4a7362a1f48
Piyush Raj <164622139+piyushraj04@users.noreply.github.com>
2026-03-23 16:44:58 +0530

    Patterns

--- Arrays/Patterns/First.java
+++ Arrays/Patterns/First.java
@@ -3,13 +3,37 @@ import java.util.Scanner;
 public class First {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 'n' value :->>>>>>>>");
         int n = sc.nextInt();
-       for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            System.out.print("*"+" ");
-        }
-        System.out.println();
 
-       }
+        // first
+        // for(int i = 1; i <= n; i++){
+        // for(int j = 1; j <= n; j++){
+        // System.out.print("*"+" ");
+        // }
+        // System.out.println();
+        // }
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*" + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
     }
 }
