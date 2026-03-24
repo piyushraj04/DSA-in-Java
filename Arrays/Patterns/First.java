620754551a8d366a8b9369224cc05cbe8c0950dc
Piyush Raj <164622139+piyushraj04@users.noreply.github.com>
2026-03-24 08:05:30 +0530

    patterns day02

--- Arrays/Patterns/First.java
+++ Arrays/Patterns/First.java
@@ -14,6 +14,7 @@ public class First {
         // System.out.println();
         // }
 
+        // second
         // for (int i = 1; i <= n; i++) {
         // for (int j = 1; j <= i; j++) {
         // System.out.print("*" + " ");
@@ -21,19 +22,152 @@ public class First {
         // System.out.println();
         // }
 
+        // 3rd-
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 4th
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 5th
+        // * * * * *
+        // * * * *
+        // * * *
+        // * *
+        // *
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = i; j <= n; j++) {
+        // System.out.print("*" + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 6th
+        // 1 2 3 4 5
+        // 2 3 4 5
+        // 3 4 5
+        // 4 5
+        // 5
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = i; j <= n; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 7th
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" " + " ");
+        // }
+        // for (int j = 1; j <= i * 2 - 1; j++) {
+        // System.out.print("*" + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 8th
+
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" " + " ");
+        // }
+        // for (int j = 1; j <= i * 2 - 1; j++) {
+        // System.out.print("*" + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 9th
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" " + " ");
+        // }
+        // for (int j = 1; j <= i * 2 - 1; j++) {
+        // System.out.print("*" + " ");
+        // }
+        // System.out.println();
+        // }
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" " + " ");
+        // }
+        // for (int j = 1; j <= i * 2 - 1; j++) {
+        // System.out.print("*" + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 10th
+
+        // for(int i = 1; i <= n;i++){
+        // if(i<=n/2+1){
+        // for(int j = 1;j<=i;j++){
+        // System.out.print("*"+" ");
+        // }
+        // }
+        // else{
+        // //i==6
+        // for(int j = 1; j <=n-i+1;j++){
+        // System.out.print("*"+" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        // 11th
         // for (int i = 1; i <= n; i++) {
         //     for (int j = 1; j <= i; j++) {
-        //         System.out.print(j + " ");
+        //         if ((i + j) % 2 == 0) {
+        //             System.out.print("1" + " ");
+        //         } else {
+        //             System.out.print("0" + " ");
+        //         }
         //     }
         //     System.out.println();
         // }
 
+        //12th
+        // for(int i = 0; i <= n; i++){
+        //     for(int j= 1; j <= i;j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     for(int j = 1;j<=(n-i)*2;j++){
+        //         System.out.print(" "+" ");
+        //     }
+        //     for(int j= i;j>=1;j--){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
 
-         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+        //13th
+        int a = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(a+++" ");
             }
             System.out.println();
         }
+        
+
     }
 }
